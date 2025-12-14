f6248cf6bf4f85c57d68faaeea7e3548c21a17b8
sushanttwayana <sushanttwayana1@gmail.com>
2025-12-14 22:05:13 +0545

    just small change

--- Langchain/6.Runnables/runnable_demo.py
+++ Langchain/6.Runnables/runnable_demo.py
@@ -1,1 +1,10 @@
-from langchain
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+import os
+from langchain_core.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field
+# from langchain_core.pydantic_v1 import BaseModel, Field
+from typing import Literal
+from langchain_core.runnables import RunnableParallel, RunnableBranch, RunnableLambda
